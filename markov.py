78bed569df043eb4627b8b2aed37ab71ba6b7948
Stephen Brennan <smb196@case.edu>
2016-03-24 09:19:57 -0400

    Modify to allow any depth to the chain.  Also, add periods to simulate sentences.

--- markov.py
+++ markov.py
@@ -9,10 +9,11 @@ import pickle
 class MarkovChain(object):
     """Markov chain class."""
 
-    def __init__(self):
+    def __init__(self, n=1):
         """Create a new markov chain."""
         self._states = {}
         self._state_counts = {}
+        self._n = n
 
     def train(self, state, next):
         """Train the state, next state pair."""
@@ -30,7 +31,7 @@ class MarkovChain(object):
 
         sum = 0
         for word, count in state_dict.items():
-            if sum + count > choice:
+            if sum + count >= choice:
                 return word
             sum += count
 
@@ -38,11 +39,11 @@ class MarkovChain(object):
 
     def train_words(self, words):
         """Train on the list of words."""
-        it = iter(words)
-        last = next(it)
-        for curr in it:
-            self.train(last, curr)
-            last = curr
+        words = list(words)
+        for i in range(len(words) - self._n - 1):
+            state = tuple(words[i:i+self._n])
+            next = words[i+self._n]
+            self.train(state, next)
 
     def generate(self, count, start=None):
         """Generate the given number of words."""
@@ -53,6 +54,20 @@ class MarkovChain(object):
             print("error: %sd not in word list" % start, file=sys.stderr)
             return
         return MarkovGenerator(self, start, count)
+
+    def sentence(self):
+        """Generate a (hopefully) complete sentence."""
+        candidate_starts = [s for s in self._states.keys() if s[0] == '.']
+        start = random.choice(candidate_starts)
+        sentence = list(start[1:])
+        for word in self.generate(None, start=start):
+            if word != '.':
+                sentence.append(word)
+            else:
+                break
+        string = ' '.join(sentence)
+        string = string[0].upper() + string[1:] + '.'
+        return string
 
 
 class MarkovGenerator(object):
@@ -75,7 +90,7 @@ class MarkovGenerator(object):
             raise StopIteration()
 
         retval = self._markov.next(self._previous)
-        self._previous = retval
+        self._previous = self._previous[1:] + (retval,)
 
         if self._count is not None:
             self._count -= 1
@@ -89,7 +104,10 @@ def words(file):
         # Defines words as sequences of letters, hyphens, and apostrophes.  We
         # want apostrophes because they could be part of contractions, and
         # hyphens because they're in hyphenated words.
-        for word in re.findall(r"[a-zA-Z'-]+", line):
+        #
+        # Periods are included to mark the end of a sentence.  Generation tries
+        # to form sentences.
+        for word in re.findall(r"[a-zA-Z'-]+|\.", line):
             # However, we do not want apostrophes or hyphens at the beginning
             # or end.
             word = word.strip("'-")
@@ -100,40 +118,31 @@ def words(file):
 
 def main():
     """Take arguments and perform their operations."""
-    import argparse
-    ap = argparse.ArgumentParser(description="train a Markov chain")
-    ap.add_argument('-i', dest='in_markov', type=argparse.FileType('rb'),
-                    default=None, help='input Markov chain')
-    ap.add_argument('-o', dest='out_markov', type=argparse.FileType('wb'),
-                    default=None, help='output Markov chain')
-    ap.add_argument('-T', dest='in_text', type=argparse.FileType('r'),
-                    default=None, help='train on a file')
+    from argparse import ArgumentParser, FileType
+    ap = ArgumentParser(description="train or generato from a Markov chain")
+    ap.add_argument('-i', dest='in_markov', type=FileType('rb'), default=None,
+                    help='input Markov chain')
+    ap.add_argument('-o', dest='out_markov', type=FileType('wb'), default=None,
+                    help='output Markov chain')
+    ap.add_argument('-T', dest='in_text', type=FileType('r'), default=None,
+                    help='train on a file')
     ap.add_argument('-G', dest='out_text', action='store_true',
                     default=False, help='generate to a file')
-    ap.add_argument('-w', dest='words', type=int, default=100,
-                    help='number of words to generate')
-    ap.add_argument('-s', dest='start', type=str, default=None,
-                    help='start word for generation')
+    ap.add_argument('-n', dest='n', type=int, default=1,
+                    help='number of words in state')
     args = ap.parse_args()
 
     if args.in_markov is not None:
         markov = pickle.load(args.in_markov)
         args.in_markov.close()
     else:
-        markov = MarkovChain()
+        markov = MarkovChain(n=args.n)
 
     if args.in_text is not None:
         markov.train_words(words(args.in_text))
 
     if args.out_text:
-        if args.start is not None:
-            args.start = args.start.lower()
-
-        wordlist = list(markov.generate(args.words, args.start))
-
-        if args.start is not None:
-            wordlist.insert(0, args.start)
-        print(' '.join(wordlist))
+        print(markov.sentence())
 
     if args.out_markov is not None:
         pickle.dump(markov, args.out_markov)
